356745110bd1af4641dead2dab79f5714926799d
Ellen <lidenellen@gmail.com>
2025-03-26 14:21:52 +0100

    lagt till paket, uppdaterat ignorefilen, lagt in mer innehåll på startsida och about-sidan. Lagt till styling för innehål. Fixat hamburgermenyn med hjälp av js. Uppdaterat loggan.

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+//hamburgermeny på mindre skärmar
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
